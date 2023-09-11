60b94fee7187d92f5988380ebfc1eda7655c9155
fazeeh <faseehnaqvi100@gmail.com>
2023-09-11 09:11:27 -0400

    Baby's First time using Javascript!!! Edition 1

---
+++ index.js
@@ -0,0 +1,32 @@
+//1st:
+// console.log("Bismillah");
+// window.alert("Click ok!")
+
+// //I am writing my first JS Comment!
+
+
+// /*
+// Multiline Comment
+// */
+
+
+//2nd:
+
+// let age;// declared a  unique variable
+// age = 19;
+
+// let firstName = "Faseeh";
+// let student = true;
+// age = age+1;
+
+
+// console.log("Hello ",firstName);//string
+// console.log("You are ",age, "years Old!");//number
+// console.log("Enrollment Status: ",student);//bool
+
+// document.getElementById("p1").innerHTML = "Hello " + firstName;
+// document.getElementById("p2").innerHTML = "You are " + age + " Years Old!";
+// document.getElementById("p3").innerHTML = "Your Enrollment Status is: " + student;
+
+//3rd: 
+//Arithmatic expressions:
